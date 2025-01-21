5e97147687c56f7e60898a000e478a49e9d66584
Vision <158229693+VisionMP@users.noreply.github.com>
2025-01-21 15:42:32 -0500

    Delete server/scripts/save_name.lua

--- server/scripts/save_name.lua
+++
@@ -1,6 +0,0 @@
-RegisterServerEvent('vs_savename')
-AddEventHandler('vs_savename', function()   
-   MySQL.Sync.execute('UPDATE player_data SET `name` = @name WHERE license = @id', {['@name'] = GetPlayerName(source), ['@id'] = GetPlayerIdentifier(source, 0)})
-   --local updateHealth = MySQL.Sync.execute('UPDATE money SET `name` = @name WHERE license = @id', {['@name'] = name, ['@id'] = GetPlayerIdentifier(source, 0)})     
-end)
-
